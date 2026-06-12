65f7b1c6b81efd7095bddbd10bbba717dcfb0589
Florian Willi <accounts@caad.ch>
2026-06-12 17:43:33 +0200

    deactivated the attempt to get autoshift behaviour that respects tap-or-hold decision modes

--- keyboards/bastardkb/scylla/keymaps/colemak_dh/config.h
+++ keyboards/bastardkb/scylla/keymaps/colemak_dh/config.h
@@ -1,3 +1,5 @@
 #define UNICODE_SELECTED_MODES UNICODE_MODE_WINCOMPOSE
-#define TAPPING_TERM 175
-#define PERMISSIVE_HOLD
+// #define TAPPING_TERM 175
+// #define PERMISSIVE_HOLD
+// #define HOLD_ON_OTHER_KEY_PRESS // nested keypress immediately triggers hold
+
--- keyboards/bastardkb/scylla/keymaps/colemak_dh/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/colemak_dh/keymap.c
@@ -20,29 +20,30 @@
 // Modifier for alpha keys to replace them with a mod-tap shift and key press key,
 // to make them sensitive to PERMISSIVE_HOLD and HOLD_ON_OTHER_KEY_PRESS
 // instead of using autoshift.
-#define AS(kc) MT(MOD_LSFT, kc)
+// #define AS(kc) MT(MOD_LSFT, kc)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // -----------------------------------------------------------------------
     // Layer 0 — Base (Colemak-DH)
     // -----------------------------------------------------------------------
-    // [0] = LAYOUT_split_4x6_5(
-    //     KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,       KC_F6,               KC_F7,       KC_F8,       KC_F9,       KC_F10,       KC_F11,     KC_F12,
-    //     KC_TAB,      KC_Q,        KC_W,        KC_F,        KC_P,        KC_B,                KC_J,        KC_L,        KC_U,        KC_Y,        KC_MINS,     S(KC_1),
-    //     KC_CAPS,     KC_A,        KC_R,        KC_S,        KC_T,        KC_G,                KC_M,        KC_N,        KC_E,        KC_I,        KC_O,        KC_QUOT,
-    //     KC_LGUI,     KC_Z,        KC_X,        KC_C,        KC_D,        KC_V,                KC_K,        KC_H,        KC_COMM,     KC_DOT,      S(KC_SLSH),  KC_RSFT,
-    //                                            KC_LSFT,     KC_SPC,      MO(1),               MO(2),       SC_SENT,     KC_BSPC,
-    //                                                         KC_LALT,     KC_LCTL,             KC_RCTL,     KC_ESC
-    // ),
     [0] = LAYOUT_split_4x6_5(
         KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,       KC_F6,               KC_F7,       KC_F8,       KC_F9,       KC_F10,       KC_F11,     KC_F12,
-        KC_TAB,      AS(KC_Q),    AS(KC_W),    AS(KC_F),    AS(KC_P),    AS(KC_B),            AS(KC_J),    AS(KC_L),    AS(KC_U),    AS(KC_Y),     KC_MINS,     S(KC_1),
-        KC_CAPS,     AS(KC_A),    AS(KC_R),    AS(KC_S),    AS(KC_T),    AS(KC_G),            AS(KC_M),    AS(KC_N),    AS(KC_E),    AS(KC_I),     AS(KC_O),    KC_QUOT,
-        KC_LGUI,     AS(KC_Z),    AS(KC_X),    AS(KC_C),    AS(KC_D),    AS(KC_V),            AS(KC_K),    AS(KC_H),    KC_COMM,     KC_DOT,       S(KC_SLSH),  KC_RSFT,
+        KC_TAB,      KC_Q,        KC_W,        KC_F,        KC_P,        KC_B,                KC_J,        KC_L,        KC_U,        KC_Y,        KC_MINS,     S(KC_1),
+        KC_CAPS,     KC_A,        KC_R,        KC_S,        KC_T,        KC_G,                KC_M,        KC_N,        KC_E,        KC_I,        KC_O,        KC_QUOT,
+        KC_LGUI,     KC_Z,        KC_X,        KC_C,        KC_D,        KC_V,                KC_K,        KC_H,        KC_COMM,     KC_DOT,      S(KC_SLSH),  KC_RSFT,
                                                KC_LSFT,     KC_SPC,      MO(1),               MO(2),       SC_SENT,     KC_BSPC,
                                                             KC_LALT,     KC_LCTL,             KC_RCTL,     KC_ESC
     ),
+
+    // [0] = LAYOUT_split_4x6_5(
+    //     KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,       KC_F6,               KC_F7,       KC_F8,       KC_F9,       KC_F10,       KC_F11,     KC_F12,
+    //     KC_TAB,      LT(3, KC_Q),    LT(3, KC_W),    LT(3, KC_F),    LT(3, KC_P),    LT(3, KC_B),            LT(3, KC_J),    LT(3, KC_L),    LT(3, KC_U),    LT(3, KC_Y),     KC_MINS,     S(KC_1),
+    //     KC_CAPS,     LT(3, KC_A),    LT(3, KC_R),    LT(3, KC_S),    LT(3, KC_T),    LT(3, KC_G),            LT(3, KC_M),    LT(3, KC_N),    LT(3, KC_E),    LT(3, KC_I),     LT(3, KC_O),    KC_QUOT,
+    //     KC_LGUI,     LT(3, KC_Z),    LT(3, KC_X),    LT(3, KC_C),    LT(3, KC_D),    LT(3, KC_V),            LT(3, KC_K),    LT(3, KC_H),    KC_COMM,     KC_DOT,       S(KC_SLSH),  KC_RSFT,
+    //                                            KC_LSFT,     KC_SPC,      MO(1),               MO(2),       SC_SENT,     KC_BSPC,
+    //                                                         KC_LALT,     KC_LCTL,             KC_RCTL,     KC_ESC
+    // ),
 
 
     // -----------------------------------------------------------------------
@@ -80,4 +81,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       KC_TRNS,    KC_TRNS,    TO(0),          TO(0),   KC_TRNS, KC_TRNS,
                                                   KC_TRNS,    KC_TRNS,        KC_TRNS, KC_TRNS
     ),
+    // [0] = LAYOUT_split_4x6_5(
+    //     KC_F1,       KC_F2,       KC_F3,       KC_F4,       KC_F5,       KC_F6,               KC_F7,       KC_F8,       KC_F9,       KC_F10,       KC_F11,     KC_F12,
+    //     KC_TAB,      S(KC_Q),    S(KC_W),    S(KC_F),    S(KC_P),    S(KC_B),            S(KC_J),    S(KC_L),    S(KC_U),    S(KC_Y),     UC(0x00F6),     UC(0x00FC),
+    //     KC_CAPS,     S(KC_A),    S(KC_R),    S(KC_S),    S(KC_T),    S(KC_G),            S(KC_M),    S(KC_N),    S(KC_E),    S(KC_I),     S(KC_O),    UC(0x00E4),
+    //     KC_LGUI,     S(KC_Z),    S(KC_X),    S(KC_C),    S(KC_D),    S(KC_V),            S(KC_K),    S(KC_H),    KC_COMM,     KC_DOT,       S(KC_SLSH),  KC_RSFT,
+    //                                            KC_LSFT,     KC_SPC,      MO(1),               MO(2),       SC_SENT,     KC_BSPC,
+    //                                                         KC_LALT,     KC_LCTL,             KC_RCTL,     KC_ESC
+    // ),
 };
